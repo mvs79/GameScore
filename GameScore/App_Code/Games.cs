617d99444118fe14cb2cdee590012082cb24e268
PC <m.veenhuis@gmx.de>
2016-11-02 07:57:05 +0100

    New Version

---
+++ GameScore/App_Code/Games.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GameScore.App_Code
+{
+    public class Games
+    {
+        #region Constructor
+        /// <summary>
+        /// Constructor for Games
+        /// </summary>
+        /// <param name="game"></param>
+        public Games(Game game)
+        {
+            this.Game = Game;
+        }
+        public Games()
+        {
+
+        }
+        #endregion
+        #region
+        /// <summary>
+        /// Propertie of Game
+        /// </summary>
+        private Game _Game;
+        public Game Game
+        { get { return _Game; }
+          set { _Game = value; }
+        }
+
+        /// <summary>
+        /// Propertie of GameList
+        /// </summary>
+        private List<Game> _GameList;
+        public List<Game> GameList
+        {
+            get { return _GameList = new List<Game>(); }
+            set { _GameList = new List<Game>(); }
+        }
+        #endregion
+
+    }
+}
--- GameScore/MainWindow.xaml.cs
+++ GameScore/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using GameScore.App_Code;
+using System.Diagnostics;
 
 namespace GameScore
 {
@@ -22,7 +24,48 @@ namespace GameScore
     {
         public MainWindow()
         {
-            InitializeComponent();
+
+            //Player.GetArray();
+            try
+            {
+              Player.GetDividebyZero();
+            }
+            catch (DivideByZeroException ex)
+            {
+               Console.WriteLine(ex.StackTrace);
+              
+            }
+           
+            InitializeComponent();            
+        }
+
+            public List<Game> GetGames()
+            {
+                Player p1 = new Player("Tomcat", 5, 20);
+                var score = Player.ScoreValue.HighScorer.ToString();
+                Player p2 = new Player("Martin", 10, 30);
+                Player p3 = new Player("Matthias", 20, 5);
+                Player p4 = new Player("Johannes", 10, 20);
+                Game game1 = new Game("GianaSisters", p1, p2);
+                Game game2 = new Game("Heimdall", p3, p4);
+                Games games = new Games();
+                List<Game> gamesListing = games.GameList;
+                gamesListing.Add(game1);
+                gamesListing.Add(game2);
+                return gamesListing;
+
+            }
+           
+           public Games GetGames(int value)
+           {
+            Games gamer1 = new Games();
+            return gamer1;
+            }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {   
+            //List<Game> gamesListing = this.GetGames();
+            //dataGrid1.ItemsSource = gamesListing;
         }
     }
 }
